c5f68e39d3acc8272cfd449de4db88b573cb79e6
broody <broody@gmail.com>
2025-06-23 16:00:03 -1000

    feat: slot support

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,26 @@ import {
   Connector,
 } from "@starknet-react/core";
 import { Chain, sepolia, mainnet } from "@starknet-react/chains";
-import { constants } from "starknet";
+import { constants, num, shortString } from "starknet";
 import { ControllerOptions, ProfileOptions } from "@cartridge/controller";
 import ControllerConnector from "@cartridge/connector/controller";
 import Home from "./Home";
+
+const slot: Chain = {
+  id: num.toBigInt(shortString.encodeShortString("WP_LUCKYGUESS")),
+  network: "luckyguess",
+  name: "Lucky Guess",
+  rpcUrls: {
+    default: import.meta.env.VITE_LUCKYGUESS_RPC_URL,
+    public: import.meta.env.VITE_LUCKYGUESS_RPC_URL,
+  },
+  nativeCurrency: {
+    name: "Ethereum",
+    symbol: "ETH",
+    decimals: 18,
+    address: import.meta.env.VITE_ETH_ADDRESS,
+  },
+};
 
 const provider = jsonRpcProvider({
   rpc: (chain: Chain) => {
@@ -19,29 +35,26 @@ const provider = jsonRpcProvider({
         return { nodeUrl: import.meta.env.VITE_MAINNET_RPC_URL };
       case sepolia:
         return { nodeUrl: import.meta.env.VITE_SEPOLIA_RPC_URL };
+      case slot:
+        return { nodeUrl: import.meta.env.VITE_LUCKYGUESS_RPC_URL };
       default:
         throw new Error(`Unsupported chain: ${chain.network}`);
     }
   },
 });
 
-const profile: ProfileOptions = {
-  preset: "eternum",
-  slot: "nums-mainnet-appchain",
-  namespace: "nums",
-};
+
 
 const options: ControllerOptions = {
-  ...profile,
-  defaultChainId: constants.StarknetChainId.SN_MAIN,
+  defaultChainId: shortString.encodeShortString("WP_LUCKYGUESS"),
   chains: [
     { rpcUrl: import.meta.env.VITE_MAINNET_RPC_URL },
     { rpcUrl: import.meta.env.VITE_SEPOLIA_RPC_URL },
+    { rpcUrl: import.meta.env.VITE_LUCKYGUESS_RPC_URL },
   ],
   tokens: {
     erc20: [import.meta.env.VITE_NUMS_ERC20],
   },
-  url: "https://keychain-git-retry-disable-sp.preview.cartridge.gg",
 };
 
 const connectors = [new ControllerConnector(options) as never as Connector];
@@ -50,7 +63,7 @@ export const App = () => {
   return (
     <StarknetConfig
       autoConnect
-      chains={[mainnet, sepolia]}
+      chains={[slot]}
       connectors={connectors}
       provider={provider}
       explorer={voyager}
